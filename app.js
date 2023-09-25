38cdc89df55f6fd3dca1e656f4a344d2e1bf8bc0
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-09-25 21:25:47 +0900

    notion 활동사진
    데이터를 json 형태로 잘 가져옴

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ require('dotenv').config();
 const app = express();
 const port = 3001;
 
-// CORS 설정
-app.use(cors({
-    origin: 'https://seron.info' // 프론트엔드 도메인
-}));
-
-// Notion API 기본 설정
+// 환경 변수에서 정보 불러오기
 const NOTION_API_KEY = process.env.NOTION_API_KEY;
 const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 const NOTION_ENDPOINT = 'https://api.notion.com/v1';
 
+// CORS 설정: 프론트엔드 도메인 기반 제한
+app.use(cors({
+    origin: 'https://seron.info'
+}));
+
+// Axios 기본 설정
 axios.defaults.headers.common['Authorization'] = `Bearer ${NOTION_API_KEY}`;
 axios.defaults.headers.common['Notion-Version'] = '2022-06-28';
 
@@ -24,19 +25,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Notion 활용 라우트
+// Notion API를 사용하여 활동 사진 데이터 가져오기
 app.get('/api/notion/pictures', async (req, res) => {
     try {
-        const response = await axios.get(`${NOTION_ENDPOINT}/databases/${NOTION_DATABASE_ID}/query`); 
-        // Notion에서 데이터를 가져오는 로직
+        const response = await axios.post(`${NOTION_ENDPOINT}/databases/${NOTION_DATABASE_ID}/query`, {});
         res.json(response.data);
     } catch (error) {
+        console.error('Error fetching data from Notion:', error);
         res.status(500).json({ message: 'Notion API에서 오류 발생' });
     }
 });
 
-// ... 나머지 라우트 및 설정
-
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
